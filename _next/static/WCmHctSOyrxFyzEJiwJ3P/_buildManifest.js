5ac99760846421a57a80622b05ba9ed2640560e7
swedenland <swedenland@users.noreply.github.com>
2025-09-10 03:08:42 +0000

    deploy: 0e9d46565a4896a3e3b7566978719e824fa27058

---
+++ _next/static/WCmHctSOyrxFyzEJiwJ3P/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/f2d88ed9592de1b8.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/f2d88ed9592de1b8.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/27ac4a29950d2ed6.js",
+  "static/chunks/turbopack-dd2b198f70c1b7c1.js"
+])
